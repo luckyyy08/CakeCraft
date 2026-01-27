0d37081fb27c61233b7b29133fd2d8db90e25ad4
Lokesh Ahire <lokeshahire85@gmail.com>
2026-01-27 17:38:53 +0530

    chnagws

--- index.php
+++ index.php
@@ -126,7 +126,17 @@ session_start();
         </div>
 
         <div class="owl-carousel testimonial-carousel">
-            <!-- same testimonial code (unchanged) -->
+            <div class="testimonial-item bg-dark text-white border-inner p-4">
+                    <div class="d-flex align-items-center mb-3">
+                        <img class="img-fluid flex-shrink-0" src="img/testimonial-1.jpg" style="width: 60px; height: 60px;">
+                        <div class="ps-3">
+                            <h4 class="text-primary text-uppercase mb-1">Client Name</h4>
+                            <span>Profession</span>
+                        </div>
+                    </div>
+                    <p class="mb-0">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
+                    </p>
+                </div>
         </div>
     </div>
 </div>
